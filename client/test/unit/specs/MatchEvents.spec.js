534878a62c5e092ce6c8120b07e87274c89ab4ce
Peter <pedrojfiorito@gmail.com>
2018-06-26 22:54:38 -0300

    Basic rendering unit tests

---
+++ client/test/unit/specs/MatchEvents.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import MatchEvents from '@/components/MatchEvents'
+
+describe('Home.vue', () => {
+  it('should render Match Events', () => {
+    const Constructor = Vue.extend(MatchEvents)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('h3').textContent)
+      .to.equal('Match Events')
+  })
+})
---
+++ client/test/unit/specs/MatchSummary.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import MatchSummary from '@/components/MatchSummary'
+
+describe('Home.vue', () => {
+  it('should render Match Stats', () => {
+    const Constructor = Vue.extend(MatchSummary)
+    const vm = new Constructor().$mount()
+    expect(vm.$el.querySelector('h1').textContent)
+      .to.equal('Match Stats')
+  })
+})
